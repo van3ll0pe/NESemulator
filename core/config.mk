bac4c4e4a141e9611616fcd8a716d41a72207e06
van3ll0pe <van3ll0pe.chell@gmail.com>
2025-05-12 12:44:33 +0200

    Add transfert instructions

---
+++ core/config.mk
@@ -0,0 +1,4 @@
+GLISS_PREFIX	=../../gliss2
+WITH_DISASM		= 1	# comment it to prevent disassembler building
+WITH_SIM		= 1	# comment it to prevent simulator building
+#WITH_DYNLIB	= 1	# uncomment it to build dynamicaly linkable library
